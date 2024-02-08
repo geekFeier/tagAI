6bcc697441261e84e18ab0c9b6ec9ee28036be3f
geekfeier <1831454438@qq.com>
2024-02-08 15:21:00 +0800

    url 区分本地和线上

--- js/tag_data.js
+++ js/tag_data.js
@@ -35,7 +35,13 @@ window.globalData = {
 }
 console.log('加载JSON文件')
 let file = ['人物', '人物发型', '动作', '动物装饰', '场景道具', '抽卡机', '新服饰', '服饰', '艺术魔法', '起手式', '面部表情', '颜色'];
-let url = `${window.location.origin}/${window.location.pathname}`;
+let url = ''
+if (window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1')) {
+  url = '../'
+} else {
+  url = `${window.location.origin}/${window.location.pathname}`;
+}
+
 file.forEach((item) => {
   fetch(`${url}json/${item}.json`)
     .then(response => response.json())
