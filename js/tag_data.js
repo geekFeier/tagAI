91ce766073042c2e7a74e62ba01d47a10b526525
geekfeier <1831454438@qq.com>
2024-02-08 17:16:55 +0800

    请求顺序修改

--- js/tag_data.js
+++ js/tag_data.js
@@ -1,19 +1,23 @@
-  window.globalData = {
-  }
+window.globalData = {
+}
 
-  let url = ''
-  if (window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1')) {
-    url = '../'
-  } else {
-    url = `${window.location.origin}/${window.location.pathname}`;
-  }
+let url = ''
+if (window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1')) {
+  url = '../'
+} else {
+  url = `${window.location.origin}/${window.location.pathname}`;
+}
 
-  let file = ['人物','人物发型', '动作', '动物装饰', '场景道具', '抽卡机', '新服饰', '服饰', '艺术魔法', '起手式', '面部表情', '颜色'];
-  file.forEach(async item => {
-    await fetch(`${url}json/${item}.json`)
-      .then(response => response.json())
-      .then(data => {
-        Object.assign(window.globalData, data)
-      })
-      .catch(error => console.error('加载JSON文件时出错:', error));
-  })
+let file = ['人物', '人物发型', '动作', '动物装饰', '场景道具', '抽卡机', '新服饰', '服饰', '艺术魔法', '起手式', '面部表情', '颜色'];
+
+file.forEach(async item => {
+  await fetch(`${url}json/${item}.json`)
+    .then(res => res.json())
+    .then(data => {
+      Object.assign(window.globalData, data)
+    })
+    .catch(error => console.error('加载JSON文件时出错:', error));
+})
+
+
+ 
