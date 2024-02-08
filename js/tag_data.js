9f451c69e018ce729accd93c2fbf8b206ff09b72
geekfeier <1831454438@qq.com>
2024-02-08 15:12:26 +0800

    请求 url 修改

--- js/tag_data.js
+++ js/tag_data.js
@@ -36,7 +36,7 @@ window.globalData = {
 console.log('加载JSON文件')
 let file = ['人物', '人物发型', '动作', '动物装饰', '场景道具', '抽卡机', '新服饰', '服饰', '艺术魔法', '起手式', '面部表情', '颜色'];
 file.forEach((item) => {
-  fetch(`../json/${item}.json`)
+  fetch(`${window.location.href}json/${item}.json`)
     .then(response => response.json())
     .then(data => {
       Object.assign(window.globalData, { [item]: data })
